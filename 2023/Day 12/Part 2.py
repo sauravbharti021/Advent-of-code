78c101484442df00f4a8a6c0ea5e870995bf142a
Saurav Bharti <86503989+sauravbharti021@users.noreply.github.com>
2023-12-12 11:47:33 +0530

    Create Part 2.py

---
+++ 2023/Day 12/Part 2.py
@@ -0,0 +1,109 @@
+import functools
+import math
+import collections
+
+from queue import Queue
+
+
+path = "input.txt"
+
+s="" 
+try:
+    with open(path) as file:
+        s=(file.read())
+except Exception as e:
+    print(e)
+
+arr=[]
+arr= s.split('\n')
+
+n= len(arr)
+
+mx=0
+
+answers=[]
+for i in arr:
+    x=i.split(' ')
+    mx=max(mx, len(x[0]))
+
+    temp= x[1]
+    nums=temp.split(',')
+
+    lol=""
+    for k in range(4):
+        lol+= '?'
+        lol+= x[0]
+        
+
+    x[0]+= lol
+
+    for k in range(len(nums)):
+        nums[k]= int(nums[k])
+
+    d_num=nums.copy()
+    for o in range(4):
+        for v in d_num:
+            nums.append(v)
+            
+    st= x[0]
+    a= nums
+    
+    @functools.cache
+    def recur(l, r):
+
+        # print(l,r)
+        if(l>=len(st)):
+            if(r>=len(a)):
+                return 1
+            return 0
+        
+        if(r>=len(a)):
+            for k in range(l, len(st)):
+                if(st[k]=='#'):
+                    return 0
+                
+            return 1
+        
+        ans=0
+
+        if(st[l]=='#'):
+            if(l+a[r]>len(st)):
+                return 0
+            for k in range(l, l+a[r]):
+                if(st[k]=='.'):
+                    return 0
+            
+            if(l+a[r]<len(st) and st[l+a[r]]=='#'):
+                return 0
+                
+            ans+= recur(l+a[r]+1, r+1)
+
+        elif(st[l]=='?'):
+
+            if(l+a[r]>len(st)):
+                return 0
+            
+            flag=0
+            if(l+a[r]<len(st) and st[l+a[r]]=='#'):
+                flag=1
+            
+            for k in range(l, l+a[r]):
+                if(st[k]=='.'):
+                    flag=1
+                    break
+            
+            if(flag==1):
+                ans+= recur(l+1, r)
+            else:
+                ans+= recur(l+1, r) + recur(l+a[r]+1, r+1)
+
+        else:
+            ans+= recur(l+1, r)
+
+        
+        return ans
+    
+    answers.append(recur(0, 0))
+
+
+print(sum(answers))
